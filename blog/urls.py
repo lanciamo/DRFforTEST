06a48beccde7a7815257d1aaeb5495bd46751e2e
Leonid Kashin <lkashin@gmail.com>
2023-09-02 15:11:12 +0400

    looking for pages and functionalites

--- blog/urls.py
+++ blog/urls.py
@@ -7,13 +7,19 @@ from .views import *
 urlpatterns = [
     path('', BlogAPIList.as_view()),
     path('drf-auth/', include('rest_framework.urls')),
-    path('<int:id>/posts/', PostsOfBlogAPIList.as_view()),
-    path('post_create/', PostAPICreate.as_view()), # we need to make view to create!!!
-    path('post_update/<int:pk>/', PostAPIUpdate.as_view()), # read and update post
-    path('post_delete/<int:pk>/', PostAPIDestroy.as_view()), # read and delete
     path('auth/', include('djoser.urls')),  # https://djoser.readthedocs.io/en/latest/base_endpoints.html
     re_path(r'^auth/', include('djoser.urls.authtoken')),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    #blogs
+    path('list/', BlogAPIList.as_view()),
+    path('create/', BlogAPIList.as_view()),
+    path('update/', BlogAPIUpdate.as_view()),
+    path('delete/', BlogAPIDestroy.as_view()),
+    path('<int:id>/posts/', PostsOfBlogAPIList.as_view()),
+    #posts
+    path('post_create/', PostAPICreate.as_view()), # we need to make view to create!!! and insert blog id to add post
+    path('post_update/<int:pk>/', PostAPIUpdate.as_view()), # read and update post
+    path('post_delete/<int:pk>/', PostAPIDestroy.as_view()), # read and delete
     ]
